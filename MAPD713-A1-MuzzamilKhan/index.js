70d6a5466ad21a9c29a05b2c18a1f87f05592f6a
KhanMuzz <khanmuzzamil@hotmail.com>
2020-10-11 21:44:44 -0300

    Implemented post to add new data and get()

--- MAPD713-A1-MuzzamilKhan/index.js
+++ MAPD713-A1-MuzzamilKhan/index.js
@@ -15,7 +15,7 @@ var counterPost = 0
 //Bring in needed libraries and frameworks 
 var restify = require('restify')
 
-//Set up framework save to provide data persistence in memory for products
+//Set up framework 'save'to store data in collections called products
 var products_Save = require('save')('products')
 
 //Create restify server
@@ -25,7 +25,6 @@ var myServer = restify.createServer({ name: SERVER_NAME});
 myServer.listen(PORT, HOST_IP, function(){
         console.log("----- WELCOME TO "+myServer.name + "  @  " + myServer.url)
         console.log("----- The server is ready for your requests at: http://"+HOST_IP +":"+PORT)
-        console.log("----- Resources: /products , /products.id")
 });
 
 //Allow use of Post requests
@@ -33,3 +32,42 @@ myServer.use(restify.fullResponse())
 
 //Map both body of Request and Params coming in with it -Unable switching between
 myServer.use(restify.bodyParser())
+
+//Post method to create new product and store it in memory in collections 'products'
+myServer.post('/products', function(req, resp, next){
+        //increment post counter
+        counterPost ++
+        console.log("The current post counter is: " + counterPost)
+        //fetch param data and make a new 
+        var newProduct = {
+            product : req.params.product,
+            price : req.params.price,
+            category: req.params.category
+        }
+
+        //save this user in memory using 'save' FW to save it under products 
+        products_Save.create(newProduct, function(error, currProductAdded){
+
+            if(error)
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            
+ 
+            //send back current addition if no errors
+            resp.send(201, currProductAdded)
+        })//persistence ends
+});//add a user post ends
+
+// Print all products
+myServer.get('/products', function(req, resp, next){
+
+        //using .find() to get all data stored in products collections
+        products_Save.find({}, function(error, productsDataObject){
+            if(error){
+                console.log(error)
+            }
+            resp.send(productsDataObject)
+        })
+     /// resp.send("heyyyy")
+
+
+})//get all user ends
